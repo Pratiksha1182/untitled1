c022ac9f0afb6b62f480481833520acf095f1659
Pratiksha <pratikshapatel1182@gmail.com>
2020-03-11 18:56:46 +0000

    untitled1

---
+++ src/ArrayHomework070320/Ex2.java
@@ -0,0 +1,20 @@
+package ArrayHomework070320;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        List<String> list_Strings = new ArrayList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        System.out.println(list_Strings);
+
+
+    }
+}
+
+
